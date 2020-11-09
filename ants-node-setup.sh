6162e89cc688fb7ac73655b860add768955fd827
Ivar T Hovden <djloek@gmail.com>
2020-11-09 17:56:50 +0100

    improve NREC scripts

--- ants-node-setup.sh
+++ ants-node-setup.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
-​
 # ants-node-*
 # Openstack customization script
 # Norwegian Research and Education Cloud (NREC)
 # https://docs.nrec.no/intro.html#about
 # Author: Ivar Thokle Hovden
-​
 # Each instance created with the script should have their 
 # own public ip address
-
 # Use ubuntu 18.04 LTS, since it is the only Ubuntu distro to data (2020-11-09) to have ants available
-​
 # Set time zone
 #export TZ="Europe/Oslo"
 echo "sudo dpkg-reconfigure tzdata" > /home/ubuntu/postinstall-manual.sh
 
 # Install software
 apt-get update -y && apt-get upgrade -y
-apt-get install -y nload ants
+apt-get install -y tmux nload ants
 
 git clone https://github.com/ivartz/ants-bcond /home/ubuntu/ants-bcond
--- distribute-data.sh
+++ distribute-data.sh
@@ -1,3 +1,7 @@
+# First, with run_evals=0
+# cd /cygdrive/z/code/ants-bcond
+# bash distribute-data.sh >> "/cygdrive/z/derivatives/cancer-sim-SAILOR_PROCESSED_MNI-01-distributed/distributed-log.txt"
+
 # Copy data to ants nodes from the machine with SSH access
 
 #dataset_dist="/mnt/HDD3TB/derivatives/cancer-sim-SAILOR_PROCESSED_MNI-01-distributed"
@@ -9,7 +13,8 @@ run_evals=1
 
 readarray -t splits < <(find $dataset_dist -mindepth 1 -maxdepth 1 -type d)
 
-ips=("158.39.77.109")
+# NREC Ubuntu 18.04 LTS VMs
+ips=("158.39.77.86" "158.39.77.109" "158.39.77.161" "158.39.77.209")
 
 num_ips=${#ips[*]}
 
@@ -19,10 +24,17 @@ num_splits=${#splits[*]}
 for ((i=0; i<num_ips; i++)); do
     ip=${ips[$i]}
     data=${splits[$i]}
-    c="rsync -avh --info=progress2 -e 'ssh -i /home/ivar/craiivar.key' $data/* ubuntu@$ip:/home/ubuntu/data"
-    echo $c
+    c="ssh-keygen -R $ip"
     if [ $run_evals == 1 ]; then
         eval $c
+    else
+        echo $c
+    fi
+    c="rsync -avh --info=progress2 -e 'ssh -o StrictHostKeyChecking=no -i /home/ivar/craiivar.key' $data/* ubuntu@$ip:/home/ubuntu/data"
+    if [ $run_evals == 1 ]; then
+        eval $c
+    else
+        echo $c
     fi
 done
 
