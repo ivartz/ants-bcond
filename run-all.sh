0d99aa18e8accbf3d16be9052a06fcd28a705fc5
Ivar T Hovden <djloek@gmail.com>
2020-11-23 21:50:14 +0100

    final collect script

--- run-all.sh
+++ run-all.sh
@@ -1,10 +1,10 @@
 dataset="/mnt/HDD3TB/derivatives/cancer-sim-SAILOR_PROCESSED_MNI-01"
 
-bash synregister-cc-models-dataset.sh
-bash synregister-mi-models-dataset.sh
+#bash synregister-cc-models-dataset.sh
+#bash synregister-mi-models-dataset.sh
 
-bash analyse-syn-cc-dataset.sh
-bash analyse-syn-mi-dataset.sh
+#bash analyse-syn-cc-dataset.sh
+#bash analyse-syn-mi-dataset.sh
 
 bash collect-syn-cc-estimation-error-segments.sh 0 > $dataset/syn-cc-estimation-error-segments.txt
 bash collect-syn-mi-estimation-error-segments.sh 0 > $dataset/syn-mi-estimation-error-segments.txt
